66b1234822bf26f211a29969abd3da88320c176c
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-09 21:40:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 function selectDifferentDrivers(drivers, option) {
-  return option
+  return option(drivers)
 }
